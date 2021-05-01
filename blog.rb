b42dbc97baea6fcbcee7549447ef5e3445bf06e0
J.J.Traders <77620375+janarthan269@users.noreply.github.com>
2021-05-01 14:00:28 +0530

    Delete blog.rb

--- blog.rb
+++
@@ -1,6 +0,0 @@
-$ ruby -r rubygems -e 'require "jekyll-import";
-    JekyllImport::Importers::Blogger.run({
-      "source"                => "/1/blog-05-01-2021.xml",
-      "no-blogger-info"       => false, # not to leave blogger-URL info (id and old URL) in the front matter
-      "replace-internal-link" => false, # replace internal links using the post_url liquid tag.
-    })'
